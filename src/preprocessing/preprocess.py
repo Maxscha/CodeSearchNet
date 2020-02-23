a4acfd0dfa1f5d9a9f7e0f2cdcb6fe62f481fba4
Maximilian Schall <maxscha@mail.de>
2020-02-23 21:05:28 +0100

    Add stop word removal and lemmization

--- src/preprocessing/preprocess.py
+++ src/preprocessing/preprocess.py
@@ -1,3 +1,19 @@
 from .ngram import make_to_ngram
+import nltk
+# Load library
+from nltk.corpus import stopwords
+
+# You will have to download the set of stop words the first time
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('wordnet')
+nltk.download('stopwords')
+
 def preprocess(text):
-    return make_to_ngram(text, 3)
+    sno = nltk.stem.SnowballStemmer('english')
+    stop_words = set(stopwords.words('english'))
+    wordnet_lemmatizer = WordNetLemmatizer()
+
+    text = list([wordnet_lemmatizer.lemmatize(x) for x in text if x not in stop_words])
+    # return make_to_ngram(text, 3)
+    return text
